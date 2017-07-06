b02b328403adfd908dcee0ab9eeec61e2ec1e813
maxbausher <maxbausher@gmail.com>
2017-07-06 15:02:33 +1000

    successfully built out flight seating chart on show page, currently working through css for chart and evenutally need to add click functionality onto the seating chart

--- burning-airlines/app/assets/javascripts/views/SeatingChartView.js
+++ burning-airlines/app/assets/javascripts/views/SeatingChartView.js
@@ -5,17 +5,31 @@ app.SeatingChartView = Backbone.View.extend({
     el: '#detailsContainer',
 
     render: function(){
-        var rawTemplate = $('#seatingChart').html();
-        var template = _.template( rawTemplate);
-        // debugger;
-        var markup = template( this.model.attributes );
-        this.$el.html( markup );
-        console.log(markup);
+        // console.log('this.model', this.model);
 
-        var plane = this.model.attributes
-        var scv = new app.SeatSchematicView({
-            model: plane
+        var rawTemplate = $('#seatingChart').html();
+        var template = _.template( rawTemplate );
+        var markup = template( this.model.attributes );
+        var rowTotal = this.model.attributes.airplane.row;
+        var colTotal = this.model.attributes.airplane.column;
+        var rows = _.range(1, rowTotal+1);
+        var columns = _.range(1, colTotal+1);
+        var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        var $seatTable = $('<table class="seatTable teal lighten-2"/>');
+        console.log('$seatTable start:', $seatTable);
+        _.each(rows, function(num){
+            var $row = $("<tr>");
+            $row.appendTo($seatTable);
+            _.each(columns, function(n){
+                var $column = $('<td class= "seat">').attr('row_no', num).attr('column_no', letters[n-1]);
+                $column.appendTo($row);
+            });
         });
+        // debugger;
+        this.$el.html( $seatTable );
+
+        // this.$el.html("HELLO");
+
 
     },
 
